7678e72adee933f51ca61ecebfb157f73625c07b
Beau Bouchard <beau@beaubouchard.com>
2013-10-12 01:33:56 -0400

    Create app.js

---
+++ worldmap/src/js/app.js
@@ -0,0 +1,45 @@
+/*
+ * Game Engine for Sprite Based HTML5 Canvas Game
+ * Author: Beau Bouchard (@beaubouchard)
+ */
+ 
+ 
+
+
+//+------------------------------------------------+
+//| 		GAME 								   |
+//+------------------------------------------------+
+
+var Game = {
+	initialize: function() {
+	
+	},
+	onready: function() { 
+	
+	},
+	onstart: function() {
+	},
+	renderToCanvas: function(){
+	},
+	parseImage: function(image, callback) {
+	
+	},
+	setupLevel: function(source) {
+	}
+}
+//+------------------------------------------------+
+//| 		MAP  								   |
+//+------------------------------------------------+
+var Map = Class.create({
+	initialize: function() {
+	
+	},
+}); // Map
+
+var Monster = Class.create({
+	initialize: function() {
+	
+	},
+  });
+	
+	
