ed205b691938bdce3698e49a121125eba74135ec
Beau Bouchard <beau@beaubouchard.com>
2013-10-13 12:06:41 -0400

    Update app.js

--- worldmap/src/js/app.js
+++ worldmap/src/js/app.js
@@ -3,57 +3,148 @@
  * Author: Beau Bouchard (@beaubouchard)
  */
  
- 
+var keyStroke = {};
+var tick = 0;
+var game;
 
-
+function messagelog(text){
+	console.log("## " + text);
+}
 //+------------------------------------------------+
 //| 		GAME 				   |
 //+------------------------------------------------+
 
-var Game = {
+function Game(){
+	this.map;
+	this.player;
+}
+Game.prototype = {
 	initialize: function() {
-	
+		messagelog("game.prototype init");
+		this.map = new Map();
+		this.map.initialize();
+		addEventListener("keydown", function (e) {
+				keyStroke[e.keyCode] = true;
+			}, false);
+		addEventListener("keyup", function (e) {
+				delete keyStroke[e.keyCode];
+			}, false);
 	},
 	onready: function() { 
 	
 	},
-	onstart: function() {
+	onstart: 			function() {
 	},
-	renderToCanvas: function(){
+	renderToCanvas: 	function(){
 	},
-	parseImage: function(image, callback) {
+	parseImage: 		function(image, callback) {
 	
 	},
-	setupLevel: function(source) {
+	setupLevel: 		function(source) {
 		
+	},
+	//+------------------------------------------------+
+	//| 		Game loop			   |
+	//+------------------------------------------------+
+	//Game loop
+	main: 				function(){
+		var now = Date.now();
+		var delta = now - then;
+	
+		game.update(delta / 1000);
+		game.render();
+	
+		then = now;
+	},
+	update: 			function(inc_mod){
+		game.handleInput(inc_mod);
+	},
+	render:				function(){
+	},
+	//+------------------------------------------------+
+	//| 		INPUT 				   |
+	//+------------------------------------------------+
+	// Event listener for keystroke codes
+	handleInput:		function(inc_mod){
+			if ((38 in keyStroke )|| (87 in keyStroke)) {// up key stroke or 'w' key stroke
+				if(cdetect){player.y -= player.speed * inc_mod;}
+				else{player.y += player.speed * inc_mod*4;}
+				tick += inc_mod*5;
+			}
+			if ((40 in keyStroke) || (83 in keyStroke)) { // down key stroke or 's' key stroke
+				if(cdetect){player.y += player.speed * inc_mod;}
+				else{player.y -= player.speed * inc_mod*4;}
+				tick += inc_mod*5;
+			}
+			if ((37 in keyStroke) || (65 in keyStroke)){ // left key stroke or 'a' key stroke
+				if(cdetect){player.x -= player.speed * inc_mod;}
+				else{player.x += player.speed * inc_mod*4;}
+				tick += inc_mod*5;
+			}
+			if ((39 in keyStroke) || (68 in keyStroke)) { // right key stroke or 'd' key stroke
+				if(cdetect){player.x += player.speed * inc_mod;}
+				else{player.x -= player.speed * inc_mod*4;}
+				tick += inc_mod*5;
+			}
+			
+			if(tick >1){playerAnimate();tick=0;}
+			
+			//action, or fire? button 
+			if ((32 in keyStroke) || (17 in keyStroke)) { // SPACE key stroke or CTRL key stroke
+				//Fire
+			}
+			// PLAYER VARIABLES
 	}
 }
 //+------------------------------------------------+
 //| 		MAP  				   |
 //+------------------------------------------------+
-var Map = Class.create({
+function Map(){
+	this.canvas;
+}
+Map.prototype = {
 	initialize: function() {
-	 	var canvas = document.createElement("canvas");
-	 	var context= canvas.getContext("2d");
-	 	canvas.width = 1024;
-	 	canvas.height = 768;
-	 	document.getElementById("game").appendChild(canvas);
+		messagelog("map.prototype init");
+		this.canvas = document.createElement("canvas");
+	 	var context= this.canvas.getContext("2d");
+	 	this.canvas.width = 1024;
+	 	this.canvas.height = 768;
+	 	document.getElementById("game").appendChild(this.canvas);
+		
 	},
-}); // Map
+	getCanvas: function() { 
+		return this.canvas;
+	}
+} // Map
 
 //+------------------------------------------------+
 //| 		Monster 			   |
 //+------------------------------------------------+
-var Monster = Class.create({
+function Monster (){
+}
+Monster.prototype = {
 	initialize: function() {
 	
 	},
-  });
+	spawn: function(ix, iy) {
+		
+	},
+	render: function() {
+		
+	},
+	tryMove: function(dX, dY) {
+		
+	}
+ }
   
 //+------------------------------------------------+
 //| 		Player	 			   |
 //+------------------------------------------------+
-var Player = Class.create({
+function Player(){
+	this.x;
+	this.y;
+}
+Player.prototype = {
 	initialize: function(canvas) {
 		this.x = canvas.width / 2;
 		this.y = canvas.height / 2;
@@ -68,87 +159,36 @@ var Player = Class.create({
 	tryMove: function(dX, dY) {
 		
 	}
-});
+}
 
 //+------------------------------------------------+
 //| 		Entity	 			   |
 //+------------------------------------------------+
 // other non-moving items. Including barriers/grass
-var Entity = Class.create({
-//	render
+function Entity(){
+}
+Entity.prototype = {
 	initialize: function() {
 	
 	},
 	render: function() {
 		
 	}
-	
-});
-
-
-//+------------------------------------------------+
-//| 		INPUT 				   |
-//+------------------------------------------------+
-// Event listener for keystroke codes
-var keyStroke = {};
-
-addEventListener("keydown", function (e) {
-	keyStroke[e.keyCode] = true;
-	}, false);
-addEventListener("keyup", function (e) {
-		delete keyStroke[e.keyCode];
-	}, false);
- 
- var tick = 0;
- 
- // event action for each keystroke
-function handleInput(inc_mod) {
-	if ((38 in keyStroke )|| (87 in keyStroke)) {// up key stroke or 'w' key stroke
-		if(cdetect){player.y -= player.speed * inc_mod;}
-		else{player.y += player.speed * inc_mod*4;}
-		tick += inc_mod*5;
-	}
-	if ((40 in keyStroke) || (83 in keyStroke)) { // down key stroke or 's' key stroke
-		if(cdetect){player.y += player.speed * inc_mod;}
-		else{player.y -= player.speed * inc_mod*4;}
-		tick += inc_mod*5;
-	}
-	if ((37 in keyStroke) || (65 in keyStroke)){ // left key stroke or 'a' key stroke
-		if(cdetect){player.x -= player.speed * inc_mod;}
-		else{player.x += player.speed * inc_mod*4;}
-		tick += inc_mod*5;
-	}
-	if ((39 in keyStroke) || (68 in keyStroke)) { // right key stroke or 'd' key stroke
-		if(cdetect){player.x += player.speed * inc_mod;}
-		else{player.x -= player.speed * inc_mod*4;}
-		tick += inc_mod*5;
-	}
-	
-	if(tick >1){playerAnimate();tick=0;}
-	
-	//action, or fire? button 
-	if ((32 in keyStroke) || (17 in keyStroke)) { // SPACE key stroke or CTRL key stroke
-		//Fire
-	}
-	// PLAYER VARIABLES
 }
 
-//+------------------------------------------------+
-//| 		Game loop			   |
-//+------------------------------------------------+
-//Game loop
-var main = function () {
-	var now = Date.now();
-	var delta = now - then;
-	
-	update(delta / 1000);
-	render();
-	
-	then = now;
-};
-start();
+
+
+
+ 
+
+
+
+
+game = new Game();
+game.initialize();
+
 var then = Date.now();
-setInterval(main,1);
-var update = function (inc_mod) { handleInput(inc_mod); }
+setInterval(game.main(),1);
+
 	
 	
